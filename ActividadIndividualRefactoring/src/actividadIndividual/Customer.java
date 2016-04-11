e8ac5f6fc6d9e924ae2c058b0680577de5491839
Jose Luis <jose-luis-navarro-motos@hotmail.com>
2016-04-11 17:38:08 +0200

    Moving the Amount Calculation

--- ActividadIndividualRefactoring/src/actividadIndividual/Customer.java
+++ ActividadIndividualRefactoring/src/actividadIndividual/Customer.java
@@ -25,17 +25,15 @@ class Customer {
 		Enumeration rentals = _rentals.elements();
 		String result = "Rental Record for " + getName() + "\n";
 		while (rentals.hasMoreElements()) {
-			double thisAmount = 0;
 			Rental each = (Rental) rentals.nextElement();
-			thisAmount = amountFor(each);
 			// add frequent renter points
 			frequentRenterPoints++;
 			// add bonus for a two day new release rental
 			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
 				frequentRenterPoints++;
 			// show figures for this rental
-			result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
-			totalAmount += thisAmount;
+			result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
+			totalAmount += each.getCharge();
 		}
 		// add footer lines
 		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
@@ -44,23 +42,6 @@ class Customer {
 	}
 
 	private double amountFor(Rental aRental) {
-		double result = 0;
-		switch (aRental.getMovie().getPriceCode()) {
-		case Movie.REGULAR:
-			result += 2;
-			if (aRental.getDaysRented() > 2)
-				result += (aRental.getDaysRented() - 2) * 1.5;
-			break;
-		case Movie.NEW_RELEASE:
-			result += aRental.getDaysRented() * 3;
-			break;
-		case Movie.CHILDRENS:
-			result += 1.5;
-			if (aRental.getDaysRented() > 3)
-				result += (aRental.getDaysRented() - 3) * 1.5;
-			break;
-		}
-		return result;
-
+		return aRental.getCharge();
 	}
 }
